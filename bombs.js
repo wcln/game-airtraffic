c2d1fb6d1bb80a030e67afcf916053f4a5597b53
Colin Bernard <colinjbernard@hotmail.com>
2017-03-12 01:19:29 -0800

    taking off now works, as well as game over screen

--- bombs.js
+++ bombs.js
@@ -1,17 +1,17 @@
 /**
  * BCLearningNetwork.com
  * Air Traffic
- * @author Colin Bernard
+ * @author Colin Bernard (colinjbernard@hotmail.com)
  * March 2017
  */
 
-//////////////////// VARIABLES
+
 
 // unicode characters
 var EXPONENT_2 = "\u00B2";
 var SQUARE_ROOT = "\u221A";
 
-// settings (may be changed)
+///////////////// THESE SETTINGS MAY BE CHANGED WITHOUT TOO MUCH CONSEQUENCE ///////////////////////////////
 var questions =  [SQUARE_ROOT+"64", SQUARE_ROOT+"81", SQUARE_ROOT+"25", "9"+EXPONENT_2];
 var answers = [8, 9, 5, 81];
 var PLANE_MIN_SPEED = 1;
@@ -20,45 +20,46 @@ var PLANE_FLY_AWAY_SPEED = 15;
 var MAX_TIME_BETWEEN_LANDING_PLANE_SPAWN = 6000; // 6 seconds
 var MIN_TIME_BETWEEN_LANDING_PLANE_SPAWN = 1000; // 1 second
 var FPS = 40;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// unicode characters
-var EXPONENT_2 = "\u00B2";
-var SQUARE_ROOT = "\u221A";
+//////////////////// VARIABLES ////////////////////////////////////
 
-
-var lastPlaneSpawnedTime;
-
-var mute = false;
+// preloadJS
 var manifest;
 var preload;
 var progressText;
 var startText;
-var scoreText;
-var planeImages = []; // array of all the possible plane images
-var questionCounter;
 
+// HTML elements
 var userInput; // user input text box
 var questionElement; // selected question
+var mute = false;
 
+// utility
 var gameStarted = false;
-
-
 var score = 0;
+var scoreText;
 
 // constants (set in init function)
 var STAGE_WIDTH;
 var STAGE_HEIGHT;
 
+// planes
 var landingPlanes = []; // array of all the plane objects which spawn in the sky and want to land
-var takeoffPlanes = []; // array of all the plane objects which spawn on the ship and want to take off
-var selectedPlane;
+var selectedPlane; // the plane that is currently selected by the player
+var numberOfPlanesTakingOff = 0; // ensures that only one plane is taking off at a time
+var planeImages = []; // array of all the possible plane images
+var lastPlaneSpawnedTime; // last time that a plane was spawned
+var questionCounter; // keeps track of questions which have game has gone through
 
-// plane object
-var planeObject = new Object();
-
-var graphics = new createjs.Graphics(); // init graphics object used for drawing selected rectangles
+// graphics object
+var graphics = new createjs.Graphics(); 
 graphics.setStrokeStyle(1);
-var box;
+var box; // used for drawing selected rectangle
+
+  //////////////////////////////
+ //			FUNCTIONS		 //
+//////////////////////////////
 
 
 /**
@@ -76,6 +77,7 @@ function init() {
 	stage.enableMouseOver(); // Default, checks the mouse 20 times/second for hovering cursor changes
 
 	questionCounter = 0;
+	score = 0; // reset score
 
 	userInput = document.getElementById("user-input"); // userInput.value gets value of input textbox
 	questionElement = document.getElementById("question"); // the question <p> element
@@ -89,7 +91,7 @@ function init() {
 
 	// add loading progress text (used by preload)
 	progressText = new createjs.Text("", "20px Arial", "#000000");
-	progressText.x = 300 - progressText.getMeasuredWidth() / 2;
+	progressText.x = STAGE_WIDTH/2 - progressText.getMeasuredWidth() / 2;
 	progressText.y = 20;
 	stage.addChild(progressText);
 	stage.update();
@@ -105,45 +107,59 @@ function init() {
 function tick(event) {
 	if (gameStarted) {
 
-		if (box != null) {
-			box.x -= landingPlanes[questionCounter].speed;
+		// spawn a new plane
+		if ((new Date().getTime() - lastPlaneSpawnedTime > Math.floor(Math.random() * MAX_TIME_BETWEEN_LANDING_PLANE_SPAWN) + MIN_TIME_BETWEEN_LANDING_PLANE_SPAWN) && questionCounter + 1 <= questions.length) {
+			
+			if ((Math.floor(Math.random() * 5) + 1) == 4 && numberOfPlanesTakingOff == 0) { // 1 in 5 chance to spawn a plane on the runway
+				setupTakingOffPlane(landingPlanes[questionCounter]);
+			} else {
+				landingPlanes[questionCounter].flying = true;
+			}
+			questionCounter++;
+			lastPlaneSpawnedTime = new Date().getTime();
 		}
 
-		landingPlanes[questionCounter].bitmap.x -= landingPlanes[questionCounter].speed; // move the plane to the left
-		landingPlanes[questionCounter].label.x = landingPlanes[questionCounter].bitmap.x + landingPlanes[questionCounter].width/2 - landingPlanes[questionCounter].label.getMeasuredWidth()/2; // center label over plane
+		var gameover = true; // check if there are no more planes flying or taking off
+
+		// update all the landing planes
+		for (var i = 0; i < landingPlanes.length; i++) {
+			if (landingPlanes[i].flying) {
+				gameover = false;
+				landingPlanes[i].bitmap.x -= landingPlanes[i].speed; // move the plane to the left
+				landingPlanes[i].label.x = landingPlanes[i].bitmap.x + landingPlanes[i].width/2 - landingPlanes[i].label.getMeasuredWidth()/2; // center label over plane
+				if (landingPlanes[i].bitmap.x + landingPlanes[i].width < 0) {
+					if (!landingPlanes[i].solved) { // if the plane left the screen without the question being solved
+						updateScore(-100);
+						stage.removeChild(landingPlanes[i].label); // remove the label from the stage as well
+						landingPlanes[i].flying = false; // plane is no longer flying and will no longer be updated
+						stage.removeChild(landingPlanes[i].bitmap);
+
+						if (landingPlanes[i].selected) { // if the plane that left the screen was the selected one
+							resetSelectedPlane(); 
+						}
+					}
+				}
 
 
-		
-		
-		if (landingPlanes[questionCounter].bitmap.x + landingPlanes[questionCounter].width < 0) {
-
-			if (!landingPlanes[questionCounter].solved) { // if the plane left the screen without the question being solved
-				updateScore(-100);
-				stage.removeChild(landingPlanes[questionCounter].label);
-				resetSelectedPlane();
-
-			}
-
-			if (questionCounter < questions.length - 1) {
-				questionCounter++;
-				//planeObject.bitmap.x = STAGE_WIDTH;
-				//setupPlanes();
-
-			} else { // no more questions... game is over
-
-				//endGame();
+			// update the taking off plane
+			} else if (landingPlanes[i].takingOff) { 
+				gameover = false;
 
 			}
 		}
 
-
-
-
-
-
-
+		// update selected box position
+		if (box != null && selectedPlane.flying) {
+			box.x -= selectedPlane.speed;
+		}
 	
 	}
+
+	// detects when game is over (see gameover boolean variable)
+	if (gameover) {
+		endGame();
+	}
+
 	stage.update(event);
 }
 
@@ -180,9 +196,13 @@ function loadError(evt) {
 
 function handleFileProgress(event) {
     progressText.text = (preload.progress*100|0) + " % Loaded";
+    progressText.x = STAGE_WIDTH/2 - progressText.getMeasuredWidth() / 2;
     stage.update();
 }
 
+/*
+ * Displays the start screen.
+ */
 function loadComplete(event) {
     console.log("Finished Loading Assets");
 
@@ -197,6 +217,9 @@ function loadComplete(event) {
 
 //////////////////////////////////////////////////////////////////////////////// END PRELOADJS FUNCTIONS
 
+/*
+ * Starts the game.
+ */
 function startGame(event) {
 	event.remove();
 	//ticker calls update function, set the FPS
@@ -209,10 +232,21 @@ function startGame(event) {
 	lastPlaneSpawnedTime = MAX_TIME_BETWEEN_LANDING_PLANE_SPAWN; // spawn a plane immediately
 }
 
+/*
+ * Displays the end game screen.
+ */
 function endGame() {
 	gameStarted = false;
-	alert("Game Complete! Score: " + score);
-	init();
+	var gameOverText = new createjs.Text("Game Over! Score: " + score, "50px Arial");
+	var playAgainText = new createjs.Text("Click to play again", "40px Arial");
+    gameOverText.x = STAGE_WIDTH/2 - gameOverText.getMeasuredWidth()/2;
+    gameOverText.y = STAGE_HEIGHT/2 - gameOverText.getMeasuredHeight()/2;
+    playAgainText.x = STAGE_WIDTH/2 - playAgainText.getMeasuredWidth()/2;
+    playAgainText.y = STAGE_HEIGHT/2 - playAgainText.getMeasuredHeight()/2 + 70;
+	stage.addChild(gameOverText);
+	stage.addChild(playAgainText);
+	stage.update();
+	stage.on("stagemousedown", init);
 }
 
 /*
@@ -246,6 +280,8 @@ function setupPlanes() {
 			question: questions[i],
 			answer: answers[i],
 			solved: false,
+			flying: false, // should the plane be updated by tick method
+			takingOff: false, // is the plane taking off? (set to true if plane spawns on runway)
 			speed: Math.floor(Math.random() * PLANE_MAX_SPEED) + PLANE_MIN_SPEED,
 			label: new createjs.Text(questions[i], "20px Arial", "#000000")
 		});
@@ -255,9 +291,10 @@ function setupPlanes() {
 		landingPlanes[i].bitmap.x = STAGE_WIDTH;
 		landingPlanes[i].bitmap.y = Math.floor(Math.random() * 300) + 50; // between 50 and 300
 		landingPlanes[i].label.x = STAGE_WIDTH;
-		landingPlanes[i].label.y = landingPlanes[i].bitmap.y - landingPlanes[i].label.getMeasuredHeight(); // this will not be updated later
+		landingPlanes[i].label.y = landingPlanes[i].bitmap.y - landingPlanes[i].label.getMeasuredHeight(); // this will not be updated later (unless plane is taking off)
 
 
+		// plane click action listener
 		landingPlanes[i].bitmap.on("click", function Timmy(event) {
 			if (box != null) {
 				stage.removeChild(box); // remove box if there is previously one
@@ -284,38 +321,74 @@ function setupPlanes() {
 	}
 }
 
+/*
+ * Sets attributes of a plane object such that it is in "take off" mode
+ */
+function setupTakingOffPlane(plane) {
+	plane.bitmap.alpha = 0; // set plane to not visible
+	plane.takingOff = true;
+	plane.bitmap.x = STAGE_WIDTH/2;
+	plane.bitmap.y = 460;
+	plane.label.x = plane.bitmap.x + plane.width/2 - plane.label.getMeasuredWidth()/2; // center label over plane
+	plane.label.y = plane.bitmap.y - plane.label.getMeasuredHeight();
+	plane.bitmap.regX = plane.width/2;
+	plane.bitmap.scaleX = -1;
+	plane.bitmap.regX = plane.width;
+	numberOfPlanesTakingOff++;
+	createjs.Tween.get(plane.bitmap).to({alpha:1}, 1000); // fade plane in
+}
+
 
 /*
  * Enter button is pressed
  */
 function enterPressed() {
+	if (gameStarted) {
+		if (selectedPlane == null) { // no plane is selected
+			alert("Please select (click) on a plane!");
+		} else {
+			if (parseInt(userInput.value) == selectedPlane.answer) { // correct answer
 
-	if (selectedPlane == null) { // no plane is selected
-		alert("Please select (click) on a plane!");
-	} else {
-		if (parseInt(userInput.value) == selectedPlane.answer) { // correct answer
+				updateScore(100);
+				stage.removeChild(selectedPlane.label);
+				stage.removeChild(box);
+				//planeObject.speed = PLANE_FLY_AWAY_SPEED;
+				selectedPlane.solved = true;
 
-		updateScore(100);
-		stage.removeChild(landingPlanes[questionCounter].label);
-		stage.removeChild(box);
-		//planeObject.speed = PLANE_FLY_AWAY_SPEED;
-		landingPlanes[questionCounter].solved = true;
-		land(landingPlanes[questionCounter]); // animation of the plane landing
+				if (selectedPlane.flying) {
+					land(selectedPlane); // animation of the plane landing
+				} else if (selectedPlane.takingOff) {
+					takeOff(selectedPlane);
+				}
+		
+			} else { // wrong answer
+				updateScore(-50);
 
-	} else { // wrong answer
-		updateScore(-50);
-
+			}
+			clearTextbox(); // clear the user input field
+		}
 	}
-
-	
-	clearTextbox(); // clear the user input field
-	}
-
-
 }
 
 /*
- * Animation of plane landing on the ship.
+ * Animation of plane taking off from the runway.
+ */
+function takeOff(plane) {
+
+	createjs.Tween.get(plane.bitmap)
+		.to({x:plane.bitmap.x + 150, rotation: -20}, 1700) // go down runway
+		.to({x:plane.bitmap.x + 1000, y:plane.bitmap.y - 500}, 6000) // lift off
+		.call( function() {
+			stage.removeChild(plane.bitmap);
+			plane.takingOff = false;
+			numberOfPlanesTakingOff--;
+		});
+
+	resetSelectedPlane();
+}
+
+/*
+ * Animation of plane landing on the runway.
  * @param plane: the plane object
  */
 function land(plane) {
@@ -332,8 +405,9 @@ function land(plane) {
 		.to({x:STAGE_WIDTH/2 - 100, y:450}, 1000)
 		.to({x:STAGE_WIDTH/2, y: 460}, 1000)
 		.to({alpha:0}, 1000) // fade plane out
-		.call(function() {
+		.call( function() {
 			stage.removeChild(plane.bitmap); // remove plane from stage
+			plane.flying = false; // plane will no longer be updated
 		});
 	
 	resetSelectedPlane();
@@ -343,7 +417,7 @@ function land(plane) {
  * Sets no plane selected.
  */
 function resetSelectedPlane() {
-	selectedPlane = null;
+	//selectedPlane = null;
 	questionElement.innerHTML = "";
 }
 
