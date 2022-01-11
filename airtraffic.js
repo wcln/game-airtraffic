ccd3f2863b0a668d2e8536354bd914f2f30ad297
Colin Perepelken <colin@perepelken.ca>
2022-01-10 18:04:23 -0800

    Update jquery, bootstrap, and createjs sources

--- airtraffic.js
+++ airtraffic.js
@@ -1,7 +1,7 @@
 /**
- * BCLearningNetwork.com
+ * wcln.ca
  * Air Traffic
- * @author Colin Bernard (colinjbernard@hotmail.com)
+ * @author Colin Perepelken (colin@perepelken.ca)
  * March 2017
  */
 
